2d09e1e0ae3fc7e58663e77360a1a5429bf472b8
mehdi khorrami <mehdikhorrami90@gmail.com>
2025-08-05 13:20:25 +0200

    Final Version

--- static/js/client.js
+++ static/js/client.js
@@ -123,6 +123,7 @@ document.addEventListener("DOMContentLoaded", () => {
             });
             messageInput.value = "";
             messageInput.style.height = 'auto';
+            messageInput.style.height = '38px'
         }
     }
 
